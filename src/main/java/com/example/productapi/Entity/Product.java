d2b9d14e7dcbc7e93d45c19c43616cb882420ef3
Amos-developer <amosnyoni185@gmail.com>
2025-07-25 17:54:08 +0300

    Configure PostresSQL DB and creating Entity DB

--- src/main/java/com/example/productapi/Entity/Product.java
+++ src/main/java/com/example/productapi/Entity/Product.java
@@ -1,4 +1,54 @@
 package com.example.productapi.Entity;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.*;
+
+@Entity
 public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private Double price;
+    private Integer quantity;
+
+    public Product(String name, Double price, Integer quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public Product() {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+
+
+
+
+
+
+
 }
