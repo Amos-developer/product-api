94b37d93ad90d909ac565ebb45a2d25ee5c3a4e0
Amos-developer <amosnyoni185@gmail.com>
2025-07-26 00:40:40 +0300

    Implementing and adding Controller

---
+++ src/main/java/com/example/productapi/Contoller/ProductController.java
@@ -0,0 +1,37 @@
+package com.example.productapi.Contoller;
+
+import com.example.productapi.Entity.Product;
+import com.example.productapi.Service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    @PostMapping("/create")
+    public Product create(@RequestBody Product product){
+        return (Product) productService.createProduct(product);
+    }
+
+    @GetMapping("/read")
+    public List<Product> getAll(){
+        return productService.getAllProducts();
+    }
+
+    @GetMapping("/read/{id}")
+    public Product getProduct(@PathVariable Long id){
+        return productService.getProductById(id);
+    }
+
+    @PutMapping("/update/{id}")
+    public Product update(@PathVariable Long id, @RequestBody Product product){
+        return productService.updateProduct(id, product);
+    }
+
+}
